c21ce3e3e079cb4f3c5b74c7eab7e71eeb80b649
Alan Christensen <alan.christensen@familysearch.org>
2016-04-02 21:37:56 -0600

    Change from 4 spaces to 2

--- extension/myscript.js
+++ extension/myscript.js
@@ -1,80 +1,80 @@
 function getUserName() {
-	return document
-		.querySelector('#nav-display-name')
-		.text;
+  return document
+    .querySelector('#nav-display-name')
+    .text;
 }
 
 function getCollectionTitle() {
-	return document
-		.querySelector(".collectionTitle a")
-		.text;
+  return document
+    .querySelector(".collectionTitle a")
+    .text;
 }
 
 function getUri() {
-	return window.location.href;
+  return window.location.href;
 }
 
 function getBreadcrumb() {
-	var breadcrumb = "";
+  var breadcrumb = "";
 
-	var elements = document
-		.querySelector('.breadcrumbDiv')
-		.querySelectorAll('div span.text');
+  var elements = document
+    .querySelector('.breadcrumbDiv')
+    .querySelectorAll('div span.text');
 
-	for (var i = 0; i < elements.length; i++) {
-		item = elements.item(i);
-		breadcrumb += item.innerText + ' ';
-	}
+  for (var i = 0; i < elements.length; i++) {
+    item = elements.item(i);
+    breadcrumb += item.innerText + ' ';
+  }
 
-	return breadcrumb.trim();
+  return breadcrumb.trim();
 }
 
 function getPageAndImage() {
-	var page = getPage();
+  var page = getPage();
 
-	return 'Image: ' + getImage() + 
-		((!!page) ? ' Page: ' + page : ''); 
+  return 'Image: ' + getImage() + 
+    ((!!page) ? ' Page: ' + page : ''); 
 }
 
 function getPage() {
-	return document
-		.querySelector('.pageNum input')
-		.value;
+  return document
+    .querySelector('.pageNum input')
+    .value;
 }
 
 function getImage() {
-	return document
-		.querySelector('.imageNum input')
-		.value;
+  return document
+    .querySelector('.imageNum input')
+    .value;
 }
 
 function getCitation() {
-	return getCollectionTitle() + '\n' + 
-		getBreadcrumb() + '\n' + 
-		getPageAndImage();
+  return getCollectionTitle() + '\n' + 
+    getBreadcrumb() + '\n' + 
+    getPageAndImage();
 }
 
 
 function isFamilySearch() {
-	return window.location.hostname.indexOf("familysearch") >= 0;
+  return window.location.hostname.indexOf("familysearch") >= 0;
 }
 
 function isAncestry() {
-	return window.location.hostname.indexOf("ancestry") >= 0;
+  return window.location.hostname.indexOf("ancestry") >= 0;
 }
 
 if (isAncestry()) {
-	chrome.runtime.sendMessage({
-		action: "sourceInfo",
-		title: getCollectionTitle(),
-		uri: getUri(),
-		citation: getCitation(),
-	});
+  chrome.runtime.sendMessage({
+    action: "sourceInfo",
+    title: getCollectionTitle(),
+    uri: getUri(),
+    citation: getCitation(),
+  });
 }
 
 if (isFamilySearch()) {
-	chrome.runtime.sendMessage({
-		action: "selectedText",
-		selectedText: window.getSelection().toString().trim()
-	});
+  chrome.runtime.sendMessage({
+    action: "selectedText",
+    selectedText: window.getSelection().toString().trim()
+  });
 }
