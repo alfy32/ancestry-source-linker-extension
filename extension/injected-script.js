cd9d8a258bb6d227c3cda0c6c1d19f5bd00483b9
Alan Christensen <alan.christensen@familysearch.org>
2016-04-12 21:46:14 -0600

    Cleanup injected script.
    Put all the code in an immediately invoked function.
    Create a function to get the PID from FamilySearch
    - Added a regex to make sure it only gets PIDs

--- extension/injected-script.js
+++ extension/injected-script.js
@@ -1,80 +1,93 @@
-function getUserName() {
-  return document
-    .querySelector('#nav-display-name')
-    .text;
-}
+(function () {
 
-function getCollectionTitle() {
-  return document
-    .querySelector(".collectionTitle a")
-    .text;
-}
-
-function getUri() {
-  return window.location.href;
-}
-
-function getBreadcrumb() {
-  var breadcrumb = "";
-
-  var elements = document
-    .querySelector('.breadcrumbDiv')
-    .querySelectorAll('div span.text');
-
-  for (var i = 0; i < elements.length; i++) {
-    item = elements.item(i);
-    breadcrumb += item.innerText + ' ';
+  function getCollectionTitle() {
+    return document
+        .querySelector(".collectionTitle a")
+        .text;
   }
 
-  return breadcrumb.trim();
-}
+  function getUri() {
+    return window.location.href;
+  }
 
-function getPageAndImage() {
-  var page = getPage();
+  function getBreadcrumb() {
+    var breadcrumb = "";
 
-  return 'Image: ' + getImage() + 
-    ((!!page) ? ' Page: ' + page : ''); 
-}
+    var elements = document
+        .querySelector('.breadcrumbDiv')
+        .querySelectorAll('div span.text');
 
-function getPage() {
-  return document
-    .querySelector('.pageNum input')
-    .value;
-}
+    var item;
+    for (var i = 0; i < elements.length; i++) {
+      item = elements.item(i);
+      breadcrumb += item.innerText + ' ';
+    }
 
-function getImage() {
-  return document
-    .querySelector('.imageNum input')
-    .value;
-}
+    return breadcrumb.trim();
+  }
 
-function getCitation() {
-  return getCollectionTitle() + '\n' + 
-    getBreadcrumb() + '\n' + 
-    getPageAndImage();
-}
+  function getPageAndImage() {
+    var page = getPage();
 
+    return 'Image: ' + getImage() +
+        ((!!page) ? ' Page: ' + page : '');
+  }
 
-function isFamilySearch() {
-  return window.location.hostname.indexOf("familysearch") >= 0;
-}
+  function getPage() {
+    return document
+        .querySelector('.pageNum input')
+        .value;
+  }
 
-function isAncestry() {
-  return window.location.hostname.indexOf("ancestry") >= 0;
-}
+  function getImage() {
+    return document
+        .querySelector('.imageNum input')
+        .value;
+  }
 
-if (isAncestry()) {
-  chrome.runtime.sendMessage({
-    action: "sourceInfo",
-    title: getCollectionTitle(),
-    uri: getUri(),
-    citation: getCitation(),
-  });
-}
+  function getCitation() {
+    return getCollectionTitle() + '\n' +
+        getBreadcrumb() + '\n' +
+        getPageAndImage();
+  }
 
-if (isFamilySearch()) {
-  chrome.runtime.sendMessage({
-    action: "selectedText",
-    selectedText: window.getSelection().toString().trim()
-  });
-}
+  function isFamilySearch() {
+    return window.location.hostname.indexOf("familysearch") >= 0;
+  }
+
+  function isAncestry() {
+    return window.location.hostname.indexOf("ancestry") >= 0;
+  }
+
+  /**
+   * Scrape the Ancestry source page for The title and the URI of the source.
+   * This is used to prepopulate the fields in the popup.
+   */
+  if (isAncestry()) {
+    chrome.runtime.sendMessage({
+      action: "sourceInfo",
+      title: getCollectionTitle(),
+      uri: getUri(),
+      citation: getCitation()
+    });
+  }
+
+  /**
+   * Scrape the page for selected text.
+   * This allows you to highlight a PID on FamilySearch and add it to the person list.
+   */
+  if (isFamilySearch()) {
+    chrome.runtime.sendMessage({
+      action: "selectedText",
+      selectedText: getPID()
+    });
+  }
+
+  function getPID() {
+    var matches = window.getSelection().toString()
+        .match(/[A-z0-9]{4}-[A-z0-9]{3,4}/);
+
+    return matches ? matches[0] : undefined;
+  }
+
+})();
